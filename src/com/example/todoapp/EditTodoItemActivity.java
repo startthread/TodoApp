00564888fbb5274c35c6f2e4980c0d3e7ce107e9
Prakash Joshi <pjoshi@twitter.com>
2014-08-30 23:01:27 -0700

    Delete todo item on long click.

--- src/com/example/todoapp/EditTodoItemActivity.java
+++ src/com/example/todoapp/EditTodoItemActivity.java
@@ -3,14 +3,13 @@ package com.example.todoapp;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
 public class EditTodoItemActivity extends Activity {
 
-	public static final String EXTRA_TODO_ITEM = "TODO_ITEM";
+	public static final String EXTRA_TODO_ITEM = "todo_item";
+	public static final String EXTRA_ITEM_POSITION = "todo_item_position";
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -18,19 +17,21 @@ public class EditTodoItemActivity extends Activity {
 		setContentView(R.layout.activity_edit_todo_item);
 		
 		final Intent intent = getIntent();
+		final int position = intent.getIntExtra(EXTRA_ITEM_POSITION, 0);
 		final String todoItem = intent.getStringExtra(EXTRA_TODO_ITEM);
+		
 		final EditText todoEditText = (EditText) findViewById(R.id.todo_edit_text);
 		todoEditText.setText(todoItem);
+		todoEditText.setSelection(todoItem.length());
 		
 		View saveButton = findViewById(R.id.save_button);
 		saveButton.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
 				final String newTodo = todoEditText.getText().toString();
-				final Intent intent = new Intent();
-				//final Bundle data = new Bundle();
-				intent.putExtra(EXTRA_TODO_ITEM, newTodo);
-				//data.putString(EXTRA_TODO_ITEM, todoItem);
-				setResult(RESULT_OK, intent);
+				final Intent data = new Intent();
+				data.putExtra(EXTRA_TODO_ITEM, newTodo);
+				data.putExtra(EXTRA_ITEM_POSITION, position);
+				setResult(RESULT_OK, data);
 				finish();
 			}
 		});
--- src/com/example/todoapp/MainActivity.java
+++ src/com/example/todoapp/MainActivity.java
@@ -12,38 +12,34 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
-
 public class MainActivity extends Activity {
 
 	private static final String EXTRA_TODO_LIST = "todo_list";
-	
+
 	private static final int REQUEST_TODO_EDIT = 1;
-	
-	private static final int INVALID_POSITION = -1;
-	
-	private ListView mListview;
+
 	private ArrayAdapter<String> mAdapter;
 	private ArrayList<String> mTodoList;
 	private EditText mEditText;
-	private int mEditPosition = INVALID_POSITION;
-	
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
-        if (savedInstanceState != null) {
-        	mTodoList = savedInstanceState.getStringArrayList(EXTRA_TODO_LIST);
-        } else {
-        	mTodoList = new ArrayList<String>();
-        }
-        mEditText = (EditText) findViewById(R.id.todo_edit_text);
-        mListview = (ListView) findViewById(R.id.todo_list_view);
-        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mTodoList);
-        mListview.setAdapter(mAdapter);
-        
-        View addButton = findViewById(R.id.add_button);
-        addButton.setOnClickListener(new View.OnClickListener() {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		if (savedInstanceState != null) {
+			mTodoList = savedInstanceState.getStringArrayList(EXTRA_TODO_LIST);
+		} else {
+			mTodoList = new ArrayList<String>();
+		}
+		mEditText = (EditText) findViewById(R.id.todo_edit_text);
+		ListView listView = (ListView) findViewById(R.id.todo_list_view);
+		mAdapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, mTodoList);
+		listView.setAdapter(mAdapter);
+
+		View addButton = findViewById(R.id.add_button);
+		addButton.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
 				final String todo = mEditText.getText().toString().trim();
 				if (!todo.isEmpty()) {
@@ -53,17 +49,33 @@ public class MainActivity extends Activity {
 				mEditText.setText("");
 			}
 		});
-        
-        mListview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-        	public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        		mEditPosition = position;
-        		Intent intent = new Intent(MainActivity.this, EditTodoItemActivity.class);
-        		intent.putExtra(EditTodoItemActivity.EXTRA_TODO_ITEM, mAdapter.getItem(position));
-        		MainActivity.this.startActivityForResult(intent, REQUEST_TODO_EDIT);
-        	}
-		});
-    }
 
+		listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				Intent intent = new Intent(MainActivity.this,
+						EditTodoItemActivity.class);
+				intent.putExtra(EditTodoItemActivity.EXTRA_TODO_ITEM,
+						mAdapter.getItem(position));
+				intent.putExtra(EditTodoItemActivity.EXTRA_ITEM_POSITION,
+						position);
+				MainActivity.this.startActivityForResult(intent,
+						REQUEST_TODO_EDIT);
+			}
+		});
+
+		listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+			public boolean onItemLongClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				mTodoList.remove(position);
+				mAdapter.notifyDataSetChanged();
+				Toast.makeText(MainActivity.this, R.string.deleted_todo,
+						Toast.LENGTH_LONG).show();
+				return true;
+			}
+		});
+	}
+	
 	@Override
 	protected void onSaveInstanceState(Bundle outState) {
 		outState.putStringArrayList(EXTRA_TODO_LIST, mTodoList);
@@ -75,16 +87,15 @@ public class MainActivity extends Activity {
 		super.onActivityResult(requestCode, resultCode, data);
 		if (resultCode == Activity.RESULT_OK) {
 			if (requestCode == REQUEST_TODO_EDIT) {
-				final String newTodo = data.getStringExtra(EditTodoItemActivity.EXTRA_TODO_ITEM);
-				if (mEditPosition != INVALID_POSITION) {
-					mTodoList.set(mEditPosition, newTodo);
-					mAdapter.notifyDataSetChanged();
-					mEditPosition = INVALID_POSITION;
-					Toast.makeText(this, R.string.updated_todo, Toast.LENGTH_LONG).show();
-				}
+				final String newTodo = data
+						.getStringExtra(EditTodoItemActivity.EXTRA_TODO_ITEM);
+				final int position = data.getIntExtra(
+						EditTodoItemActivity.EXTRA_ITEM_POSITION, 0);
+				mTodoList.set(position, newTodo);
+				mAdapter.notifyDataSetChanged();
+				Toast.makeText(this, R.string.updated_todo, Toast.LENGTH_LONG)
+						.show();
 			}
 		}
 	}
-	
-	
 }
