911f6dcfaad12217a985f1ef2927a11799062005
Prakash Joshi <pjoshi@twitter.com>
2014-08-31 00:24:51 -0700

    Write todo list on file. Updated README

---
+++ README.md
@@ -0,0 +1,15 @@
+# TodoApp Demo
+
+
+Completed features:
+ * [x] Add todo items
+ * [x] Delete todo item on long click
+ * [x] Update todo item on click
+ 
+Notes:
+* [x] Added Toast to notify user
+* [x] File read/write operation through AsyncTask
+
+Demo GIF:
+
+![Video Walkthrough](todo.gif)
--- src/com/example/todoapp/MainActivity.java
+++ src/com/example/todoapp/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.todoapp;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+
+import org.apache.commons.io.FileUtils;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -18,24 +24,19 @@ public class MainActivity extends Activity {
 
 	private static final int REQUEST_TODO_EDIT = 1;
 
+	private final ArrayList<String> mTodoItems = new ArrayList<String>();
 	private ArrayAdapter<String> mAdapter;
-	private ArrayList<String> mTodoList;
 	private EditText mEditText;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
-		if (savedInstanceState != null) {
-			mTodoList = savedInstanceState.getStringArrayList(EXTRA_TODO_LIST);
-		} else {
-			mTodoList = new ArrayList<String>();
-		}
+		
 		mEditText = (EditText) findViewById(R.id.todo_edit_text);
 		ListView listView = (ListView) findViewById(R.id.todo_list_view);
 		mAdapter = new ArrayAdapter<String>(this,
-				android.R.layout.simple_list_item_1, mTodoList);
+				android.R.layout.simple_list_item_1, mTodoItems);
 		listView.setAdapter(mAdapter);
 
 		View addButton = findViewById(R.id.add_button);
@@ -43,8 +44,9 @@ public class MainActivity extends Activity {
 			public void onClick(View v) {
 				final String todo = mEditText.getText().toString().trim();
 				if (!todo.isEmpty()) {
-					mTodoList.add(todo);
+					mTodoItems.add(todo);
 					mAdapter.notifyDataSetChanged();
+					new SaveTodoTask().execute();
 				}
 				mEditText.setText("");
 			}
@@ -67,19 +69,16 @@ public class MainActivity extends Activity {
 		listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
 			public boolean onItemLongClick(AdapterView<?> parent, View view,
 					int position, long id) {
-				mTodoList.remove(position);
+				mTodoItems.remove(position);
 				mAdapter.notifyDataSetChanged();
+				new SaveTodoTask().execute();
 				Toast.makeText(MainActivity.this, R.string.deleted_todo,
 						Toast.LENGTH_LONG).show();
 				return true;
 			}
 		});
-	}
-	
-	@Override
-	protected void onSaveInstanceState(Bundle outState) {
-		outState.putStringArrayList(EXTRA_TODO_LIST, mTodoList);
-		super.onSaveInstanceState(outState);
+		
+		new ReadTodoTask().execute();
 	}
 
 	@Override
@@ -88,14 +87,48 @@ public class MainActivity extends Activity {
 		if (resultCode == Activity.RESULT_OK) {
 			if (requestCode == REQUEST_TODO_EDIT) {
 				final String newTodo = data
-						.getStringExtra(EditTodoItemActivity.EXTRA_TODO_ITEM);
+						.getStringExtra(EditTodoItemActivity.EXTRA_TODO_ITEM).trim();
 				final int position = data.getIntExtra(
 						EditTodoItemActivity.EXTRA_ITEM_POSITION, 0);
-				mTodoList.set(position, newTodo);
-				mAdapter.notifyDataSetChanged();
-				Toast.makeText(this, R.string.updated_todo, Toast.LENGTH_LONG)
-						.show();
+				if (!mTodoItems.get(position).equals(newTodo)) {
+					mTodoItems.set(position, newTodo);
+					mAdapter.notifyDataSetChanged();
+					new SaveTodoTask().execute();
+					Toast.makeText(this, R.string.updated_todo, Toast.LENGTH_LONG)
+							.show();
+				}
 			}
+		}
+	}
+	
+	private class SaveTodoTask extends AsyncTask<Void, Void, Void> {
+		@Override
+		protected Void doInBackground(Void... params) {
+			final File filesDir = MainActivity.this.getFilesDir();
+			final File todoFile = new File(filesDir, "todo.txt");
+			try {
+				FileUtils.writeLines(todoFile, mTodoItems);
+			} catch (IOException ex) {
+				Log.e("MainActivity", "Error while writing to todo file", ex);
+			}
+			return null;
+		}
+	}
+	
+	private class ReadTodoTask extends AsyncTask<Void, Void, Void> {
+		@Override
+		protected Void doInBackground(Void... params) {
+			final File filesDir = MainActivity.this.getFilesDir();
+			final File todoFile = new File(filesDir, "todo.txt");
+			try {
+				final ArrayList<String> listItems = new ArrayList<String>(FileUtils.readLines(todoFile));
+				mTodoItems.clear();
+				mTodoItems.addAll(listItems);
+				mAdapter.notifyDataSetChanged();
+			} catch (IOException ex) {
+				Log.e("MainActivity", "Error while reading to todo file", ex);
+			}
+			return null;
 		}
 	}
 }
